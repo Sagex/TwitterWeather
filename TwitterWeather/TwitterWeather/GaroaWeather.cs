01994c6bc0561d034fd34beda986b16191fceb2a
Sagex <jones.kapo@hotmail.com>
2021-08-25 14:44:26 -0300

    Created the GaroaWeather class and The firsts lines of the weather api. The weather API('HgBrasil') will return a json file. In the next update, i will filter some weathe infos and create a repos to environment var api key

---
+++ TwitterWeather/TwitterWeather/GaroaWeather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterWeather
+{
+    class GaroaWeather
+    {
+        public int temp { get; set; }
+        public string city { get; set; }
+        public string description { get; set; }
+        public string humidity { get; set; }
+        public string wind_speedy { get; set; }
+
+        // The command or chars: "\x0A" is to jump line. Is ASCII command
+        public override string ToString()
+        {
+            return $"Previsão do tempo da nossa querida Terra da Garoa:\x0A{city}\x0ATemperatura: {temp}°C\x0A" + $"Condição do céu: {description}\x0AUmidade: {humidity}%\x0AVelocidade do vento: {wind_speedy}";
+        }
+    }
+}
---
+++ TwitterWeather/TwitterWeather/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TwitterWeather
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string hGWeatherKey = "YOUR WEATHER KEY";
+            string hGUrl = $"?key={hGWeatherKey}";
+
+            using var Weather = new HttpClient();
+
+            Weather.BaseAddress = new Uri("https://api.hgbrasil.com/weather");
+            Weather.DefaultRequestHeaders.Add("User-Agent", "OAuth gem v0.4.4");
+            Weather.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage weatherResponse = await Weather.GetAsync(hGUrl);
+            weatherResponse.EnsureSuccessStatusCode();
+
+
+            // get Weather Response to Json obj
+            var respInJson = await weatherResponse.Content.ReadAsStringAsync();
+        }
+    }
+}
